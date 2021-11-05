6ff7e7d9139cc39c9ac5a5e7f776889409fabc70
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-04 20:35:55 -0400

    cleaned up files

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 //require and run our main fetch function
 const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// });
-fetchCoordsByIP(myIP, (error, data) => {
+fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log(data);
+  console.log('It worked! Returned IP:' , ip);
 });
+// const myIP = '';
+// fetchCoordsByIP(myIP, (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log(data);
+// });
